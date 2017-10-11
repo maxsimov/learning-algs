222dd7d410b5875331f836156da04adf94b7dfb7
Andrey Maximov <andrew.maksimov@gmail.com>
2017-10-11 22:15:34 +0000

    solved k-sorted merge

--- task-merge-k-sorted-lists.c
+++ task-merge-k-sorted-lists.c
@@ -3,7 +3,7 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <limits.h>
-
+#include "util.h"
 /* 
   Merge k Sorted Lists
   Source: https://leetcode.com/problems/merge-k-sorted-lists/description/
@@ -20,14 +20,69 @@ struct ListNode
     struct ListNode *next;
 };
 
+void sift_down(struct ListNode** lists, int listsSize, int root)
+{
+    for (int child=(2*root+2); child<listsSize; child=(2*root+2))
+    {
+        int swap_index = root;
+            
+        if (lists[swap_index]->val > lists[child]->val)
+            swap_index = child;
+                
+        ++child;
+        if (child < listsSize && lists[swap_index]->val > lists[child]->val)
+            swap_index = child;
+                
+        if (swap_index == root)
+            break;
+                
+        SWAP(lists[swap_index], lists[root]);
+        root = swap_index;
+    }
+}
+
+void advance_list(struct ListNode** lists, int *listsSize)
+{
+    lists[0] = lists[0]->next;
+    
+    if (!lists[0])
+    {
+        if (!--(*listsSize))
+            return;
+
+        lists[0] = lists[*listsSize];
+    }
+
+    sift_down(lists, *listsSize, 0);
+}
+
 struct ListNode* mergeKLists(struct ListNode** lists, int listsSize) 
 {
-    struct ListNode *result = malloc(sizeof(*result));
+    if (listsSize < 0)
+        return 0;
+
+    if (listsSize == 1)
+        return lists[0];
     
-    result->val = 0;
-    result->next = 0;
+    // heapify
+    for (int parent=(listsSize-2)/2; parent>=0; parent--)
+        sift_down(lists, listsSize, parent);
+        
+    struct ListNode *head = lists[0];
+    struct ListNode *tail = lists[0];
     
-    return result;
+    advance_list(lists, &listsSize);
+
+    while (listsSize)
+    {
+        tail->next = lists[0];
+        tail = lists[0];
+        advance_list(lists, &listsSize);
+    }
+    
+    tail->next = 0;
+
+    return head;
 }
 
 
@@ -88,10 +143,18 @@ void merge_K_lists_driver()
         printf("a[%d] ={", i);
         print_list(lists[i]);
         printf(" }\n");
-    }    
+    }
+    
+    struct ListNode *result = mergeKLists(lists, 3);
+    
+    if (!result)
+        printf("Merge failed!\n");
 
-    for (int i=0; i<sizeof(lists)/sizeof(lists[0]); ++i)
-        destroy_list(lists[i]);
+    printf("merged ={");
+    print_list(result);
+    printf(" }\n");
+    
+    destroy_list(result);
 }
 
 int main(int argc, char *argv[])
