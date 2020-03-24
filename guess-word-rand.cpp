ddb4863dc06cbe7ffe57d0db47f2cb9351abf2e8
Andrey Maximov <Andrew.Maksimov@gmail.com>
2020-03-24 20:41:32 +0000

    Added random implementation w/ filtering

--- guess-word-rand.cpp
+++ guess-word-rand.cpp
@@ -22,5 +22,33 @@ public:
 
 bool BruteForce::once = BruteForce::regSolution("BruteForce", BruteForce::createSolution<BruteForce> );
 
+class Random : public Solution
+{
+public:
+  static bool once;
+
+  void findSecretWord(vector<string>& wordlist, Master& master)
+  {
+    list<string> mayUsed(wordlist.begin(), wordlist.end());
+
+    int found = 0;
+    while (!mayUsed.empty() && found != 6)
+    {
+      string cur = mayUsed.front();
+      mayUsed.pop_front();
+
+      found = master.guess(cur);
+
+      for (auto i=mayUsed.begin(); i!=mayUsed.end(); )
+        if (match(cur, mayUsed.front()) != found)
+          i = mayUsed.erase(i);
+        else
+          i++;
+    }
+  }
+};
+
+bool Random::once = Random::regSolution("Random", Random::createSolution<Random> );
+
 
 };
